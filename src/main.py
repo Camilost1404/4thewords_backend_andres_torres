c4a214b90bc2c89fa1f201dbe3e494ce1404e478
Andrés Torres <camitorres1404@gmail.com>
2025-03-03 16:30:13 -0500

    fix: static files access and add cors

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from src.core.db.db import database
 from src.app.legends.routes import LegendRoutes
 from src.app.legends.services import LegendService
 from src.app.legends.repository import LegendRepository
+from fastapi.middleware.cors import CORSMiddleware
 
 os.makedirs(f'{settings.UPLOAD_FOLDER}', exist_ok=True)
 
@@ -20,12 +21,20 @@ legend_service = LegendService(legend_repository)
 # Routes
 legend_router = LegendRoutes(legend_service)
 
-app = FastAPI(root_path="/api")
+app = FastAPI()
 app.title = settings.app_name
 
 @app.on_event("startup")
 async def startup_event():
     database._check_connection()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/uploads", StaticFiles(directory=settings.UPLOAD_FOLDER), name="uploads")
-app.include_router(legend_router.get_router())
+app.include_router(legend_router.get_router(), prefix="/api")
